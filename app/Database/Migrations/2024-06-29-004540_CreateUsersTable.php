61f5442d35cd5a51f5968824e2f1b2c71f59722f
Beta7x <adepriyantowidies@gmail.com>
2024-08-11 12:18:24 +0700

    [MODIFY] - Change the users phone constraint

--- app/Database/Migrations/2024-06-29-004540_CreateUsersTable.php
+++ app/Database/Migrations/2024-06-29-004540_CreateUsersTable.php
@@ -24,7 +24,7 @@ class CreateUsersTable extends Migration
             ],
             'phone' => [
                 'type' => 'VARCHAR',
-                'constraint' => '15',
+                'constraint' => '20',
                 'unique' => true,
             ],
             'password' => [
